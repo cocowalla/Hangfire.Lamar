19009b93389348cebd4c2a63a86f85862271004f
cocowalla <800977+cocowalla@users.noreply.github.com>
2018-12-10 20:56:55 +0000

    Switch to `dotnet test`, as dotnet-xunit is retired

--- build.cmd
+++ build.cmd
@@ -6,9 +6,8 @@ dotnet build .\src\Hangfire.Lamar\Hangfire.Lamar.csproj --configuration Release
 rem dotnet-xunit is a CLI tool that can only be executed from in the test folder
 cd .\test\Hangfire.Lamar.Test
 
-dotnet xunit -framework netcoreapp1.1 -fxversion 1.1.2
-dotnet xunit -framework netcoreapp2.0 -fxversion 2.0.6
-dotnet xunit -framework net452
+dotnet test --framework netcoreapp2.0
+dotnet test --framework net461
 
 cd %START_DIR%
 
--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ dotnet build ./src/Hangfire.Lamar/Hangfire.Lamar.csproj --configuration Release
 # dotnet-xunit is a CLI tool that can only be executed from in the test folder
 cd ./test/Hangfire.Lamar.Test
 
-dotnet xunit -framework netcoreapp1.1 -fxversion 1.1.7
-dotnet xunit -framework netcoreapp2.0 -fxversion 2.0.5
+dotnet test --framework netcoreapp2.0
 
 cd ${TRAVIS_BUILD_DIR}
