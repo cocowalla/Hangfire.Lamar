b0fb905070b0f25188c0b57c25a0064ab43076d9
cocowalla <800977+cocowalla@users.noreply.github.com>
2018-12-10 21:34:36 +0000

    No need to switch dirs with `dotnet test`

--- build.cmd
+++ build.cmd
@@ -3,12 +3,7 @@ set START_DIR=%cd%
 dotnet restore .\Hangfire.Lamar.sln
 dotnet build .\src\Hangfire.Lamar\Hangfire.Lamar.csproj --configuration Release
 
-rem dotnet-xunit is a CLI tool that can only be executed from in the test folder
-cd .\test\Hangfire.Lamar.Test
-
 dotnet test --framework netcoreapp2.0
 dotnet test --framework net461
-
-cd %START_DIR%
 
 dotnet pack .\src\Hangfire.Lamar -c Release
--- build.sh
+++ build.sh
@@ -4,9 +4,4 @@ set -ev
 dotnet restore ./Hangfire.Lamar.sln
 dotnet build ./src/Hangfire.Lamar/Hangfire.Lamar.csproj --configuration Release
 
-# dotnet-xunit is a CLI tool that can only be executed from in the test folder
-cd ./test/Hangfire.Lamar.Test
-
 dotnet test --framework netcoreapp2.0
-
-cd ${TRAVIS_BUILD_DIR}
